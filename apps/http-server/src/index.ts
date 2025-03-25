9ad3d42f6b0fadc699d00d10cc14591054c0fd73
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-03-25 23:11:44 +0530

    some minor change in web/page.tsx

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,22 +5,24 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hi there");
-})
+  res.send("Hi there");
+});
 
 app.post("/signup", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const user = await client.user.create({
-        data: {
-            username: username,
-            password: password
-        }
-    })
-    res.json({
-        message: "Signup successful",
-        id: user.id
-    });
-})
+  console.log("req.body->",req.body)
+  const { username, password } = req.body;
+  console.log("username-", username, "password-", password);
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+
+  res.json({
+    message: "Signup successful",
+    id: user.id,
+  });
+});
 
 app.listen(3002);
